729f1178a393ad7edff6863f927c7b29d066719f
Lahiru Kaushalya Ananda <lahiruk.14@cse.mrt.ac.lk>
2018-07-14 02:02:42 +0530

    Implement user services

---
+++ NotifyMe/NotifyMe/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotifyMe.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [PrimaryKey]
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ NotifyMe/NotifyMe/ServiceInterfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using NotifyMe.Models;
+
+namespace NotifyMe.ServiceInterfaces
+{
+    public interface IUserService
+    {
+        User GetUserByEmailAsync(string Email);
+
+        IEnumerable<User> GetAllUsersAsync();
+
+        bool AddUserAsync(User user);
+
+    }
+}
